08123a3c3303a773f79121e42091a374d0c859f4
hailong11 <hailong11@staff.weibo.com>
2018-07-06 19:20:21 +0800

    update

---
+++ lib/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+uglifyjs kk.js kk.extend.js kk.data.js kk.date.js kk.event.js kk.http.js kk.app.js kk.element.js kk.element.view.js \
+    kk.element.image.js kk.element.text.js kk.element.button.js kk.element.webview.js kk.element.scrollview.js kk.element.slideview.js \
+    kk.element.animation.js kk.page.js \
+    -o ../bin/kk.min.js
